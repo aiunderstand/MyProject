20fce1c43a8bd6adb46c259d7838db6a44e66d3b
aiunderstand <steven@stevenbos.com>
2024-10-23 12:19:53 +0200

    Added folder structure for tasks and added a tasksense example

--- Code/src/main.adb
+++ Code/src/main.adb
@@ -1,4 +1,6 @@
 with MicroBit.Console; use MicroBit.Console;
+with TaskSense;
+
 use MicroBit;
 -- USN PROJECT TEMPLATE INTELLIGENT REAL-TIME SYSTEMS
 -- Project name: [project name]
@@ -8,7 +10,6 @@ use MicroBit;
 --This is a project template for a Jorvik profile (a less restrictive Ravenscar) for the MicroBit v2 such that we have a language supported real-time OS for embedded targets
 --It requires the nRF52833 Jorvik runtime files. They can be found in the folder "runtime profiles" and need to be copied to your earm-eabi compiler toolchain
 --In future version of GNATstudio this profile is automatically available and this step can be skipped (for status see: https://github.com/AdaCore/bb-runtimes/pull/67)
-
 --Check out the many examples in the ADL for the MicroBit v2 to see:
 --  1) How the various drivers can be used like the accelerometer,speaker  or wireless communication via radio
 --  2) How ADA features like the Math library and Bounded_Vectors (Containers) can be used
@@ -20,8 +21,6 @@ use MicroBit;
 procedure Main with Priority => 0 is
 
 begin
-   Put_Line (" <-- The zero means: Let's get started...");
-   Put_Line (" <-- The zero means: Let's get started...");
    Put_Line (" <-- The zero means: Let's get started...");
    loop
       null;
